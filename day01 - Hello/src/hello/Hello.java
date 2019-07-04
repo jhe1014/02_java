65dad382db1f027c2808c949adcb125a243d1459
jhe1014 <skana1014@naver.com>
2019-07-04 10:45:20 +0900

    Hello 첫 클래스 작성

---
+++ day01 - Hello/src/hello/Hello.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// 자바 기본 주석
+		// 프로그램 실행에 영향을 미치지 않음.
+		
+		System.out.println("Hello, World!");
+		
+
+	}
+
+}
