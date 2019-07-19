5fd08c71f96dbfa600ab79dfa4ca942b2d1e74d8
jhe1014 <skana1014@naver.com>
2019-07-19 09:45:46 +0900

    서점 실습

--- Day10 - Class.Book/src/book/BookShelf.java
+++ Day10 - Class.Book/src/book/BookShelf.java
@@ -57,19 +57,36 @@ public class BookShelf {
 		Book[] newBooks;
 		
 		// 1. 폐기할 책이 위치하는 인덱스 찾기
+		int index = findBookIndex(book);
 		
 		// 2. 폐기할 책의 인덱스가 -1보다 크면 폐기할 책이 있다는 의미로 판단하고 삭제로직 진입
-		
-		// 3. 폐기 안 할 책을 유지할 새 배열을 지금 배열 -1 크기로 생성
-		
-		// 4. 폐기할 인덱스가 배열 중간일 때
-		// (1) 삭제할 책 앞쪽의 책 정보는 같은 인덱스로 복사
-		// (2) 삭제할 책 뒤쪽의 남는 책정보는 현재 인덱스 -1 만큼 당겨오기
-		
-		// 5. 폐기할 인덱스가 매열 끝일 때 폐기할 책 인덱스 앞쪽까지만 새 배열에 복사
-		
-		// 6. 남는 책이 복사된 새 배열을 this book에 새로 저장
-		
+		if(index > -1) {
+			// 3. 폐기 안 할 책을 유지할 새 배열을 지금 배열 -1 크기로 생성
+			newBooks = new Book[books.length - 1];
+			
+			// 4. 폐기할 인덱스가 배열 중간일 때
+			// (1) 삭제할 책 앞쪽의 책 정보는 같은 인덱스로 복사
+			// (2) 삭제할 책 뒤쪽의 남는 책정보는 현재 인덱스 -1 만큼 당겨오기
+			
+			// 5. 폐기할 인덱스가 매열 끝일 때 폐기할 책 인덱스 앞쪽까지만 새 배열에 복사
+			if(index < books.length - 1) {
+				for(int idx = 0; idx < index; idx++) {
+					newBooks[idx] = books[idx];
+				}
+				
+				for(int ind = index; ind < newBooks.length; ind++) {
+					newBooks[ind] = books[ind + 1];
+				}
+				
+			} else {
+				for(int idx = 0; idx < index; idx++) {
+					newBooks[idx] = books[idx];
+				}
+				
+			}
+			// 6. 남는 책이 복사된 새 배열을 this book에 새로 저장
+			this.books = newBooks;
+		}
 	}
 	
 	// 책 정보 수정
@@ -89,6 +106,20 @@ public class BookShelf {
 		return findBook(book);
 	}
 	
+	// 전체 책 목록을 얻기 : getAllBooks()
+	public Book[] getAllBooks() {
+		return this.books;
+	}
+	
+	
+	/**
+	 * 매개변수 전달된 책 정보와 일치하는 일련번호를 가진 책(책 배열 : books에 있는)을
+	 * 찾아서 배열 안에 들어있는 책을 리턴
+	 * 
+	 * @param book
+	 * @return
+	 */
+	
 	private Book findBook(Book book) {
 		Book findBook = null;
 		for(int idx = 0; idx < books.length; idx++) {
@@ -103,7 +134,6 @@ public class BookShelf {
 	
 	private int findBookIndex(Book book) {
 		int index = -1;
-		Book findBook = null;
 		for(int idx = 0; idx < books.length; idx++) {
 			if(books[idx].getSequence() == book.getSequence()) {
 				// 같은 책 찾았다.
