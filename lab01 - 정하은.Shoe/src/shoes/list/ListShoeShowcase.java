5500d1c953bcd065cf8b15b56063ce3e446cccb4
jhe1014 <skana1014@naver.com>
2019-08-06 10:33:47 +0900

    파일 입출력 실습

--- lab01 - 정하은.Shoe/src/shoes/list/ListShoeShowcase.java
+++ lab01 - 정하은.Shoe/src/shoes/list/ListShoeShowcase.java
@@ -20,34 +20,90 @@ public class ListShoeShowcase implements ShoeShowcase{
 	}
 	
 	// 3. 메소드 선언
+	// 신발 정보 추가
 	@Override
 	public int add(Shoe shoe) {
-		// TODO Auto-generated method stub
-		return 0;
+		int addCnt = 0;
+		
+		if(!isExists(shoe)) {
+			shoes.add(shoe);
+			addCnt++;
+		}
+		return addCnt;
 	}
 
+	// 신발 정보 수정
 	@Override
 	public int set(Shoe shoe) {
-		// TODO Auto-generated method stub
-		return 0;
+		int setCnt = 0;
+		
+		if(isExists(shoe)) {
+			int index = -1;
+			
+			for (int idx = 0; idx < shoes.size(); idx++) {
+				if(shoes.get(idx).equals(shoe)) {
+					index = idx;
+					break;
+				}
+			}
+			
+			if(index > -1) {
+				shoes.set(index, shoe);
+				setCnt++;
+			}
+		}
+		return setCnt;
 	}
 
+	// 신발 정보 삭제
 	@Override
 	public int remove(Shoe shoe) {
-		// TODO Auto-generated method stub
-		return 0;
+		int rmCnt = 0;
+		
+		if(isExists(shoe)) {
+			if (shoes.remove(shoe)) {
+				rmCnt++;
+			}
+		}
+		return rmCnt;
 	}
 
+	// 신발 정보 조회
 	@Override
 	public Shoe get(Shoe shoe) {
-		// TODO Auto-generated method stub
-		return null;
+		Shoe findShoe = null;
+		int index = -1;
+		
+		for (int idx = 0; idx < shoes.size(); idx++) {
+			if(shoes.get(idx).equals(shoe)) {
+				index = idx;
+				break;
+			}
+		}
+		
+		if(index > -1) {
+			findShoe = shoes.get(index);
+		}
+		return findShoe;
 	}
 
+	// 신발 정보 전체 조회
 	@Override
 	public List<Shoe> getAllShoes() {
-		// TODO Auto-generated method stub
-		return null;
+		return shoes;
+	}
+	
+	// 리스트에 존재하는지 검사
+	private boolean isExists(Shoe shoe) {
+		boolean exists = false;
+		
+		for (Shoe findShoe : shoes) {
+			if(findShoe.equals(shoe)) {
+				exists = true;
+				break;
+			}
+		}
+		return exists;
 	}
 	
 }
--- lab01 - 정하은.Shoe/src/shoes/set/SetShoeShowcase.java
+++ lab01 - 정하은.Shoe/src/shoes/set/SetShoeShowcase.java
@@ -1,44 +1,78 @@
 package shoes.set;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import shoe.Shoe;
 import shoes.ShoeShowcase;
 
 public class SetShoeShowcase implements ShoeShowcase{
 	// 1. 멤버변수 선언
+	Set<Shoe> shoes;
 	
 	// 2. 생성자 선언
+	public SetShoeShowcase() {
+		shoes = new HashSet<Shoe>();
+	}
+	
+	public SetShoeShowcase(Set<Shoe> shoes) {
+		this.shoes = shoes;
+	}
 	
 	// 3. 메소드 선언
 	@Override
 	public int add(Shoe shoe) {
-		// TODO Auto-generated method stub
-		return 0;
+		int addCnt = 0;
+		
+		if(shoes.add(shoe)) {
+			addCnt++;
+		}
+		return addCnt;
 	}
 
 	@Override
 	public int set(Shoe shoe) {
-		// TODO Auto-generated method stub
-		return 0;
+		/*
+		 * Set 구조에서 set : 데이터 수정은 기존 값 제거 후 수정할 값 추가
+		 */
+		int setCnt = 0;
+		
+		if(shoes.remove(shoe)) {
+			shoes.add(shoe);
+			setCnt++;
+		}
+		return setCnt;
 	}
 
 	@Override
 	public int remove(Shoe shoe) {
-		// TODO Auto-generated method stub
-		return 0;
+		int rmCnt = 0;
+		
+		if(shoes.remove(shoe)) {
+			rmCnt++;
+		}
+		return rmCnt;
 	}
 
 	@Override
 	public Shoe get(Shoe shoe) {
-		// TODO Auto-generated method stub
-		return null;
+		Shoe findShoe = null;
+		
+		for(Shoe inShoe : shoes) {
+			if(inShoe.equals(shoe)) {
+				findShoe = inShoe;
+			}
+		}
+		return findShoe;
 	}
 
 	@Override
 	public List<Shoe> getAllShoes() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Shoe> shoeList = new ArrayList<Shoe>();
+		shoeList.addAll(shoes);
+		return shoeList;
 	}
 
 }
